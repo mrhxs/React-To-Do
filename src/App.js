0ca22aa41bc1e30e3104b4194e3b062e8e172406
mrhxs <mikeridolfo1@gmail.com>
2019-05-08 16:13:35 -0500

    adding checkbox fun

--- src/App.js
+++ src/App.js
@@ -14,13 +14,17 @@ class App extends Component {
     };
   }
   
+  toggleComplete() {
+    console.log('toggleComplete executed');
+  }
+  
   render() {
     return (
       <div className="App">
        <ul>
-       { this.state.todos.map( (todo, index) => 
-        <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
-          )}
+         { this.state.todos.map( (todo, index) => 
+             <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+             )}
        </ul>       
       </div>
     );
