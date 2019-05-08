f4f3e060a409fa81f1be28169114343c8390d214
mrhxs <mikeridolfo1@gmail.com>
2019-05-08 15:48:05 -0500

    todo SAP mod

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ToDo from './components/ToDo.js';
 
-function App() {
-  return (
-    <div className="App">
-        <ul>
-          <ToDo />
-          <ToDo />
-        </ul> 
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isCompleted: false }
+      ]
+    };
+  }
+  
+  render() {
+    return (
+      <div className="App">
+       <ul>
+       { this.state.todos.map( (todo, index) => 
+        <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+          )}
+       </ul>       
+      </div>
+    );
+  }
 }
 
 export default App;
